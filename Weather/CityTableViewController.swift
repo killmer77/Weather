b72ffa1d7e91cf52b1ab3b4a26506dec33897c54
冨田悠斗 <tomitomi23@ezweb.ne.jp>
2018-10-10 14:03:56 +0900

    dami data

---
+++ Weather/CityTableViewController.swift
@@ -0,0 +1,88 @@
+//
+//  CityTableViewController.swift
+//  Weather
+//
+//  Created by 冨田悠斗 on 2018/10/10.
+//  Copyright © 2018 kang haejoon. All rights reserved.
+//
+
+import UIKit
+
+class CityTableViewController: UITableViewController {
+    
+    var cities = ["Kusatsu"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return cities.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "city", for: indexPath)
+        cell.textLabel?.text = cities[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "detail") as? ViewController {
+            vc.cityname = cities[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -11,9 +11,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var cityname = ""
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var maxMinTempLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = cityname
+        self.weatherLabel.text = "Rainy"
+        self.tempLabel.text = "24℃"
+        self.maxMinTempLabel.text = "25℃ / 16℃"
+        self.humidityLabel.text = "49%"
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
