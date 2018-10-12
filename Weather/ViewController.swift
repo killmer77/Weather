43d0a240c66a2028ae5e9134122d9823ab5a406f
冨田悠斗 <tomitomi23@ezweb.ne.jp>
2018-10-12 14:27:33 +0900

    tired

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -22,23 +22,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=e56c905428db86b3e78bd8a5064ef029"
-        urlString = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"
-//        if let url = URL(string: urlString) {
-//            if let data = try? Data(contentsOf: url) {
-//                let json = try! JSON(data: data)
-//                print(json)
-//
-////                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
-////                    parse(json: json)
-////                    return
-////                }
-//            }else{
-//                print("error")
-//            }
-//        }else{
-//            print("error2")
-//        }
+        urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=e56c905428db86b3e78bd8a5064ef029"
+//        urlString = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"
         
         openweathermap()
 
@@ -52,14 +37,32 @@ class ViewController: UIViewController {
     }
     
     func openweathermap(){
-        // openweathermapApiを用いて各情報を取得
         let url = NSURL(string: self.urlString)!
         let task = URLSession.shared.dataTask(with: url as URL, completionHandler: {data, response, error in
-            // リソースの取得が終わると、ここに書いた処理が実行
             let json = try! JSON(data: data!)
-            print(json)
+            self.parse(json: json)
         })
         task.resume()
+    }
+    
+    func parse(json: JSON) {
+        let main = json["main"]
+        let max_k = main["temp_max"].floatValue
+        let min_k = main["temp_min"].floatValue
+        let temp_k = main["temp"].floatValue
+        let humidity = main["humidity"].stringValue
+        let weatherjson = json["weather"].arrayValue
+        let weatherjson2 = weatherjson[0]
+        let weather = weatherjson2["main"].stringValue
+        let max = Int(max_k - 273.15)
+        let min = Int(min_k - 273.15)
+        let temp = Int(temp_k - 273.15)
+        DispatchQueue.main.async {
+            self.tempLabel.text = String(temp) + "℃"
+            self.maxMinTempLabel.text = String(max) + "℃ / " + String(min) + "℃"
+            self.humidityLabel.text = humidity + "%"
+            self.weatherLabel.text = weather
+        }
     }
 
 }
