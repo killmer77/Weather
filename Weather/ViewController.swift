d628bc40ac27ee4dab45f311f23dd507a6e97aed
冨田悠斗 <tomitomi23@ezweb.ne.jp>
2018-10-12 13:34:47 +0900

    fixed

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -18,26 +18,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var maxMinTempLabel: UILabel!
+    var urlString: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let urlString: String
-        urlString = "https://api.openweathermap.org/data/2.5/weather?q={" + cityname + "}"
-        if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-                let json = try! JSON(data: data)
-                print(json)
-                
-//                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
-//                    parse(json: json)
-//                    return
-//                }
-            }else{
-                print("error")
-            }
-        }else{
-            print("error2")
-        }
+//        urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=e56c905428db86b3e78bd8a5064ef029"
+        urlString = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"
+//        if let url = URL(string: urlString) {
+//            if let data = try? Data(contentsOf: url) {
+//                let json = try! JSON(data: data)
+//                print(json)
+//
+////                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+////                    parse(json: json)
+////                    return
+////                }
+//            }else{
+//                print("error")
+//            }
+//        }else{
+//            print("error2")
+//        }
+        
+        openweathermap()
 
         title = cityname
         self.weatherLabel.text = "Rainy"
@@ -46,6 +49,17 @@ class ViewController: UIViewController {
         self.humidityLabel.text = "49%"
         
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func openweathermap(){
+        // openweathermapApiを用いて各情報を取得
+        let url = NSURL(string: self.urlString)!
+        let task = URLSession.shared.dataTask(with: url as URL, completionHandler: {data, response, error in
+            // リソースの取得が終わると、ここに書いた処理が実行
+            let json = try! JSON(data: data!)
+            print(json)
+        })
+        task.resume()
     }
 
 }
